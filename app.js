60008200010b76c70b9d51acadcacdaa78e45daf
Kenneth C <kennethalonsoc@gmail.com>
2024-06-27 13:21:10 -0600

    chore: Update app.js to handle user signup and contact form submissions (using sendgrid)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sgMail = require('@sendgrid/mail');
-require('dotenv').config();
+require('dotenv').config(); // Asegúrate de que esta línea esté presente y en la parte superior del archivo
 
 const app = express();
 const PORT = 3000;
@@ -13,6 +13,25 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/signup', (req, res) => {
+    const { username, email, password } = req.body;
+    console.log(`New user registered: ${username}, ${email}`);
+    res.send('Registration successful');
+});
+
+app.post('/contact', (req, res) => {
+    const { name, email, message } = req.body;
+    const msg = {
+        to: 'kennethalonsoc@gmail.com',
+        from: email,
+        subject: 'New Contact Message',
+        text: message,
+    };
+    sgMail.send(msg)
+        .then(() => res.send('Email sent'))
+        .catch(error => res.send('Failed to send email'));
 });
 
 app.listen(PORT, () => {
